bcc5bf830fe9bf037c0d27dd6664bfd53a1eda3b
TerlTH <121073987+TerlTH@users.noreply.github.com>
2025-06-20 14:54:54 +1000

    Update urls.py

--- main/main/urls.py
+++ main/main/urls.py
@@ -1,10 +1,34 @@
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API проекта Заметки",
+      default_version='v1',
+      description="Автогенерируемая документация к API",
+      terms_of_service="https://www.example.com/policy",
+      contact=openapi.Contact(email="support@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('LabaWeb.urls')),
     path('api-token-auth/', obtain_auth_token),
+]
+
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
